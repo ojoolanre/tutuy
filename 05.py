606c484c6f91753eb213f929823ab714539d816d
ojoolanre <43132874+ojoolanre@users.noreply.github.com>
2018-09-10 10:28:25 +0100

    Add files via upload

---
+++ 05.py
@@ -0,0 +1,17 @@
+
+shooping=["yam","bread","rice"]
+price={"yam":24,"bread":50,"rice":50}
+
+for x in shooping:
+    print(shooping)
+    print(price)
+for value in price:
+    print(price)
+
+print(shooping[1])
+print(price["rice"])
+print(sellers_no)
+print(sellers_no[1:])
+ 
+
+                
---
+++ assignment3.py
@@ -0,0 +1,9 @@
+#get the value from user
+x=int(input("give me a number"))
+if(x%3==0):
+    print("fizz")
+if(x%5==0):  
+    print("buzz")
+if(x%3==0 and x%5==0):    
+    print("fizzbuzz")
+
